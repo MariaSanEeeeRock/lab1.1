c8abe19516bbfa502ea9481ca5645aae6dbbd678
MariaSanEeeeRock <Catana006@gmail.com>
2023-10-12 11:17:01 +0500

    lab1_1

---
+++ app/src/main/java/com/example/lab1_1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        // получаем элемент textView
+        TextView textView = findViewById(R.id.header);
+        // переустанавливаем у него текст
+        textView.setText("Hello from Java!");
+    }
+}
